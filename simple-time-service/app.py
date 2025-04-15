a87166ca1243a4f6346e8fbdc54debbe401171d8
ZS\vs37090 <vijayaramarao.sirigiri@zs.com>
2025-04-16 00:43:43 +0530

    update on code

--- simple-time-service/app.py
+++ simple-time-service/app.py
@@ -1,4 +1,16 @@
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
+# from datetime import datetime, timezone
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def get_current_time():
+#     utc_now = datetime.now(timezone.utc)
+    # return jsonify({
+    #     "current_time": utc_now.isoformat()
+    # })
+
+from flask import Flask, render_template
 from datetime import datetime, timezone
 
 app = Flask(__name__)
@@ -6,6 +18,7 @@ app = Flask(__name__)
 @app.route('/')
 def get_current_time():
     utc_now = datetime.now(timezone.utc)
-    return jsonify({
-        "current_time": utc_now.isoformat()
-    })
+    return render_template('time.html', current_time=utc_now.isoformat())
+
+if __name__ == '__main__':
+    app.run(debug=True)
