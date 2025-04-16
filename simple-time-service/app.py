53f42edeac688740f5bc55cba152fdd65e70b9f7
ZS\vs37090 <vijayaramarao.sirigiri@zs.com>
2025-04-16 08:08:09 +0530

    Dockerfile path changed

--- simple-time-service/app.py
+++ simple-time-service/app.py
@@ -1,16 +1,4 @@
-# from flask import Flask, jsonify
-# from datetime import datetime, timezone
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def get_current_time():
-#     utc_now = datetime.now(timezone.utc)
-    # return jsonify({
-    #     "current_time": utc_now.isoformat()
-    # })
-
-from flask import Flask, render_template
+from flask import Flask, jsonify
 from datetime import datetime, timezone
 
 app = Flask(__name__)
@@ -18,7 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def get_current_time():
     utc_now = datetime.now(timezone.utc)
-    return render_template('time.html', current_time=utc_now.isoformat())
+    return jsonify({
+        "current_time": utc_now.isoformat()
+    })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# from flask import Flask, render_template
+# from datetime import datetime, timezone
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def get_current_time():
+#     utc_now = datetime.now(timezone.utc)
+#     return render_template('time.html', current_time=utc_now.isoformat())
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
