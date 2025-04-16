18da15716defc4f0a4ec3cbd32a66253a7762209
ZS\vs37090 <vijayaramarao.sirigiri@zs.com>
2025-04-16 10:25:18 +0530

    non-root-user

--- ecs-fargate/iam.tf
+++ ecs-fargate/iam.tf
@@ -21,6 +21,32 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# Extra permissions to ensure logs work
+resource "aws_iam_role_policy" "ecs_task_execution_logging_policy" {
+  name = "${var.app_name}-ecs-logging-policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name              = "/ecs/${var.app_name}"
+  retention_in_days = 7
+}
+
 resource "aws_iam_role" "ecs_task_role" {
   name = "${var.app_name}-ecsTaskRole"
 
@@ -45,10 +71,12 @@ resource "aws_iam_role_policy" "ecs_task_s3_access" {
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = ["s3:GetObject"],
-      Resource = "arn:aws:s3:::your-bucket-name/*"
-    }]
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["s3:GetObject"],
+        Resource = "arn:aws:s3:::your-bucket-name/*"
+      }
+    ]
   })
 }
