a5c862604077f1ee1885f1a04d2cd5440620d516
ZS\vs37090 <vijayaramarao.sirigiri@zs.com>
2025-04-16 19:13:08 +0530

    changed contianer port 8080

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "ecs_fargate" {
   region              = "us-east-1"
   domain_name         = "simpletimeservice.cloudvj.in"
   hosted_zone_id      = "Z03659932DLDYYQJTHLW"
-  container_port      = 80
+  container_port      = 8080
   image_tag = var.image_tag
 }
