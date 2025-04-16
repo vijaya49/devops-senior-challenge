68e2bb1f8c5a9d9ea1ba8230dfb7ff98b4296764
ZS\vs37090 <vijayaramarao.sirigiri@zs.com>
2025-04-16 17:45:20 +0530

    root-user

--- simple-time-service/app.py
+++ simple-time-service/app.py
@@ -1,14 +1,14 @@
-from flask import Flask, jsonify
-from datetime import datetime, timezone
+# from flask import Flask, jsonify
+# from datetime import datetime, timezone
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def get_current_time():
-    utc_now = datetime.now(timezone.utc)
-    return jsonify({
-        "current_time": utc_now.isoformat()
-    })
+# @app.route('/')
+# def get_current_time():
+#     utc_now = datetime.now(timezone.utc)
+#     return jsonify({
+#         "current_time": utc_now.isoformat()
+#     })
 
 # from flask import Flask, render_template
 # from datetime import datetime, timezone
@@ -22,3 +22,21 @@ def get_current_time():
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+from flask import Flask, jsonify, request
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_info():
+    utc_now = datetime.now(timezone.utc)
+    visitor_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    
+    return jsonify({
+        "timestamp": utc_now.isoformat(),
+        "ip": visitor_ip
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
